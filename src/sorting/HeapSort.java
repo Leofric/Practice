a88929acc9a6a93069ee48a95dab395b5d8972a6
Alex Berthon <Alexjberthon@hotmail.com>
2019-05-30 20:42:06 -0700

    Fixed inconsistency

--- src/sorting/HeapSort.java
+++ src/sorting/HeapSort.java
@@ -5,13 +5,13 @@ public class HeapSort {
         int array[] = {14, 6, 5, 9, 12, 3}; 
   
         System.out.print("Initial Array: ");
-        printArray(array);
+        print(array);
      
         HeapSort sorter = new HeapSort(); 
         sorter.sort(array); 
         
         System.out.print("Sorted Array:  ");
-        printArray(array);
+        print(array);
     } 
 	
     public void sort(int array[]){
@@ -32,31 +32,31 @@ public class HeapSort {
         } 
     } 
   
-    void heapify(int arr[], int n, int i){
+    void heapify(int array[], int n, int i){
         int largest = i;
         int l = 2*i + 1;
         int r = 2*i + 2;
   
-        if (l < n && arr[l] > arr[largest]) 
+        if (l < n && array[l] > array[largest]) 
             largest = l; 
   
-        if (r < n && arr[r] > arr[largest]) 
+        if (r < n && array[r] > array[largest]) 
             largest = r; 
   
         if (largest != i){
-            int swap = arr[i]; 
-            arr[i] = arr[largest]; 
-            arr[largest] = swap; 
+            int swap = array[i]; 
+            array[i] = array[largest]; 
+            array[largest] = swap; 
   
             // Recursively heapify the affected sub-tree 
-            heapify(arr, n, largest); 
+            heapify(array, n, largest); 
         } 
     } 
   
     // A utility function to print array of size n //
-    static void printArray(int arr[]){
-        for (int i=0; i<arr.length; ++i) 
-            System.out.print(arr[i]+" "); 
+    static void print(int array[]){
+        for (int i=0; i<array.length; ++i) 
+            System.out.print(array[i]+" "); 
         System.out.println(); 
     } 
 } 
